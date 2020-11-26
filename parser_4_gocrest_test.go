f53f1623de881307602ee83b029262378b9be4f2
Martin W. Kirst <maki@bitkings.de>
2020-11-26 09:44:52 +0100

    some little more cases

--- parser_4_gocrest_test.go
+++ parser_4_gocrest_test.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+	"github.com/corbym/gocrest/has"
 	"github.com/corbym/gocrest/is"
 	"github.com/corbym/gocrest/then"
 	"testing"
@@ -13,12 +14,12 @@ func Test_betaflight_message_can_be_parsed__with_GOCREST(t *testing.T) {
 	info := ParseFirmwareInformation("# Betaflight / SPRACINGF3EVO (SPEV) 3.4.0 Apr 17 2018 / 14:00:13 (b2c247d34) MSP API: 1.39")
 
 	then.AssertThat(t, info.FirmwareName, is.EqualTo("Betaflight"))
-	then.AssertThat(t, info.TargetName, is.EqualTo("SPRACINGF3EVO"))
+	then.AssertThat(t, info.TargetName, has.Prefix("SPRACING"))
 	then.AssertThat(t, info.TargetDetail, is.EqualTo("SPEV"))
 	then.AssertThat(t, info.Version, is.GreaterThanOrEqualTo(int64(3)))
-	then.AssertThat(t, info.ReleaseDateStr, is.EqualTo("Apr 17 2018"))
-	then.AssertThat(t, info.ReleaseTime, is.EqualTo("14:00:13"))
-	then.AssertThat(t, info.GitHash, is.EqualTo("b2c247d34"))
+	then.AssertThat(t, info.ReleaseDateStr, is.EqualToIgnoringWhitespace("Apr172018"))
+	then.AssertThat(t, info.ReleaseTime, is.AllOf(has.Prefix("14"), has.Suffix("13")))
+	then.AssertThat(t, info.GitHash, has.Length(9))
 }
 
 func Test_parsing_multiple_lines_returns_first_hit__with_GOCREST(t *testing.T) {
